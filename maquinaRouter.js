55220123cce53d2cc0d828d453f8bf206dc75479
yan0606 <yan060604@gmail.com>
2024-04-12 17:14:22 -0300

    talhoes e maquina feito

--- maquinaRouter.js
+++ maquinaRouter.js
@@ -7,6 +7,57 @@ const app = express();
 const admin = require('./firebase') ;
 const db = admin.database();
 
+function criarTabela(dados) {
+    let tabela = `<table class="table table-striped zebrado">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Marca da maquina</th>
+                            <th>modelo</th>
+                            <th>placa</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    for (let chave in dados) {
+        tabela += `<tr>
+                        <td>${chave}</td>
+                        <td>${dados[chave].marca}</td>
+                        <td>${dados[chave].modelo}</td>
+                        <td>${dados[chave].placa}</td>
+                        <td>
+                            <a class="btn btn-outline-warning" href="/maquina/editar/${chave}">Alterar</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="/maquina/excluir/${chave}">Excluir</a>
+                        </td>
+                    </tr>`;
+    }            
+    tabela += `</tbody >
+            </table > `;
+    return tabela;
+}
+
+app.get('/', (req, res) => {
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/maquina/maquina.html', (e, dados) => {
+                let tabela = "";
+                const docmaquina = db.ref("maquina");
+                docmaquina.once("value", function(snapshot){
+                    tabela = criarTabela(snapshot.val());
+                    dados = dados.toString().replace("{tabela}", tabela);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
+            });
+        });
+    });
+});
+
+
 app.get('/', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
@@ -66,51 +117,68 @@ app.get('/editar/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
             fs.readFile('src/maquina/editar_maquina.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+                let id = req.params.id;
+                const docmaquina = db.ref("maquina/"+id);
+                docmaquina.once("value", function(snapshot){
+                    let marca = snapshot.val().marca;
+                    let modelo = snapshot.val().modelo;
+                    let placa = snapshot.val().placa;
+                    dados = dados.toString().replace("{marca}", marca);
+                    dados = dados.toString().replace("{modelo}", modelo);
+                    dados = dados.toString().replace("{placa}", placa);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-// Rota da página para editar os dados de um registro de livro
 app.post('/editar', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    let marca = req.body.marca;
+    let modelo = req.body.modelo;
+    let placa = req.body.placa;
+    let docmaquina = db.ref("maquina");
+    docmaquina.child(id).update(
+        {
+            'marca': marca,
+            'modelo': modelo,
+            'placa' : placa
+        }
+    );
+    res.redirect("/maquina");
 });
-
-// Rota da página para abrir formulário para excluir um registro de um livro
 app.get('/excluir/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
             fs.readFile('src/maquina/excluir_maquina.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+                let id = req.params.id;
+                const docmaquina = db.ref("maquina/"+id);
+                docmaquina.once("value", function(snapshot){
+                    let marca = snapshot.val().marca;
+                    let modelo = snapshot.val().modelo;
+                    let placa= snapshot.val().placa;
+                    dados = dados.toString().replace("{marca}", marca);
+                    dados = dados.toString().replace("{modelo}", modelo);
+                    dados = dados.toString().replace("{placa}", placa);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-// Rota da página para excluir um registro de um livro
 app.post('/excluir', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    const docmaquina = db.ref("maquina/" + id);
+    docmaquina.remove();
+    res.redirect("/maquina");
 });
 
 module.exports = app;
--- talhoesRouter.js
+++ talhoesRouter.js
@@ -7,20 +7,60 @@ const app = express();
 const admin = require('./firebase')  ;
 const db = admin.database();
 
-// Rota da página que exibe os livros registrados no banco de dados
+
+
+function criarTabela(dados) {
+    let tabela = `<table class="table table-striped zebrado">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome do Talhao</th>
+                            <th>longitude</th>
+                            <th>latitude</th>
+                            <th>area</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    for (let chave in dados) {
+        tabela += `<tr>
+                        <td>${chave}</td>
+                        <td>${dados[chave].nome}</td>
+                        <td>${dados[chave].longitude}</td>
+                        <td>${dados[chave].latitude}</td>
+                        <td>${dados[chave].area}</td>
+                        <td>
+                            <a class="btn btn-outline-warning" href="/talhoes/editar/${chave}">Alterar</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="/talhoes/excluir/${chave}">Excluir</a>
+                        </td>
+                    </tr>`;
+    }            
+    tabela += `</tbody >
+            </table > `;
+    return tabela;
+}
+
 app.get('/', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
-          
-
             fs.readFile('src/talhoes/talhoes.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+                let tabela = "";
+                const doctalhoes = db.ref("talhoes");
+                doctalhoes.once("value", function(snapshot){
+                    tabela = criarTabela(snapshot.val());
+                    dados = dados.toString().replace("{tabela}", tabela);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
+
 
 // Rota da página para abrir formulário para inserir um novo registro d
 app.get('/novo', (req, res) => {
@@ -63,56 +103,80 @@ app.post('/novo', urlencodedParser, (req, res) => {
     });
 });
 
-// Rota da página para abrir formuário para editar os dados de um registro de livro
 app.get('/editar/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
             fs.readFile('src/talhoes/editar_talhoes.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+                let id = req.params.id;
+                const doctalhoes = db.ref("talhoes/"+id);
+                doctalhoes.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let longitude = snapshot.val().longitude;
+                    let latitude = snapshot.val().latitude;
+                    let area = snapshot.val().area;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{longitude}", longitude);
+                    dados = dados.toString().replace("{latitude}", latitude);
+                    dados = dados.toString().replace("{area}", area);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-// Rota da página para editar os dados de um registro de livro
+
 app.post('/editar', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    let nome = req.body.nome;
+    let longitude = req.body.longitude;
+    let latitude = req.body.latitude;
+    let area = req.body.area;
+    let doctalhoes = db.ref("talhoes");
+    doctalhoes.child(id).update(
+        {
+            'nome': nome,
+            'longitude': longitude,
+            'latitude' : latitude,
+            'area' : area
+        }
+    );
+    res.redirect("/talhoes");
 });
 
-// Rota da página para abrir formulário para excluir um registro de um livro
 app.get('/excluir/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
             fs.readFile('src/talhoes/excluir_talhao.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+                let id = req.params.id;
+                const doctalhoes = db.ref("talhoes/"+id);
+                doctalhoes.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let longitude = snapshot.val().longitude;
+                    let latitude = snapshot.val().latitude;
+                    let area = snapshot.val().area;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{longitude}", longitude);
+                    dados = dados.toString().replace("{latitude}", latitude);
+                    dados = dados.toString().replace("{area}", area);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-// Rota da página para excluir um registro de um livro
 app.post('/excluir', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    const doctalhoes = db.ref("talhoes/" + id);
+    doctalhoes.remove();
+    res.redirect("/talhoes");
 });
 
 module.exports = app;
