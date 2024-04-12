907b6d8b85c06c24404bc890fc915b80a9f6299a
yan0606 <yan060604@gmail.com>
2024-04-12 10:57:57 -0300

    project rastreiaAgro, add 5 CRUDs, 2 CRUD completed

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ const admin = require ('firebase-admin') ;
 
 admin.initializeApp({
     credential: admin.credential.cert('./serviceAccountKey.json'),
-    databaseURL: "https://projetocrud-469bd-default-rtdb.firebaseio.com"
+    databaseURL: "https://appnodejs-8f9ba-default-rtdb.firebaseio.com/"
 });
 
 module.exports = admin;
