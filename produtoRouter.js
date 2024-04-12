be84f2e62660d8ef409a8391761f4975dd3d7337
yan0606 <yan060604@gmail.com>
2024-04-12 11:56:04 -0300

    :sparkles: Introducing product deletion and change

--- produtoRouter.js
+++ produtoRouter.js
@@ -7,13 +7,50 @@ const app = express();
 const admin = require('./firebase');
 const db = admin.database();
 
+function criarTabela(dados) {
+    let tabela = `<table class="table table-striped zebrado">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome do produto</th>
+                            <th>descricao</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    for (let chave in dados) {
+        tabela += `<tr>
+                        <td>${chave}</td>
+                        <td>${dados[chave].nome}</td>
+                        <td>${dados[chave].descricao}</td>
+                        <td>
+                            <a class="btn btn-outline-warning" href="/produto/editar/${chave}">Alterar</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="/produto/excluir/${chave}">Excluir</a>
+                        </td>
+                    </tr>`;
+    }
+    tabela += `</tbody >
+            </table > `;
+    return tabela;
+}
+
+
 app.get('/', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/Produto/produto.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+            fs.readFile('src/produto/produto.html', (e, dados) => {
+                let tabela = "";
+                const docproduto = db.ref("produto");
+                docproduto.once("value", function (snapshot) {
+                    tabela = criarTabela(snapshot.val());
+                    dados = dados.toString().replace("{tabela}", tabela);
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
@@ -62,36 +99,59 @@ app.post('/novo', urlencodedParser, (req, res) => {
 app.get('/editar/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/Produto/editar_produto.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+            fs.readFile('src/produto/editar_produto.html', (e, dados) => {
+                let id = req.params.id;
+                const docproduto = db.ref("produto/"+id)
+                docproduto.once("value", function (snapshot) {
+                    let nome = snapshot.val().nome;
+                    let descricao = snapshot.val().descricao;
+
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{descricao}", descricao);
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                })
+              
             });
         });
     });
 });
 
+
 // Rota da página para editar os dados de um registro de livro
 app.post('/editar', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    let nome = req.body.nome;
+    let descricao = req.body.descricao;
+    let docproduto = db.ref("produto");
+    docproduto.child(id).update(
+        {
+            'nome': nome,
+            'descricao': descricao,
+        }
+    );
+    res.redirect("/produto")
 });
 
 // Rota da página para abrir formulário para excluir um registro de um livro
 app.get('/excluir/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/Produto/excluir_produto.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+            fs.readFile('src/produto/excluir_produto.html', (e, dados) => {
+                let id = req.params.id;
+                const docproduto = db.ref("produto/"+id)
+                docproduto.once("value", function (snapshot) {
+                    let nome = snapshot.val().nome;
+                    let descricao = snapshot.val().descricao;
+
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{descricao}", descricao);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                })
             });
         });
     });
@@ -99,15 +159,10 @@ app.get('/excluir/:id', (req, res) => {
 
 // Rota da página para excluir um registro de um livro
 app.post('/excluir', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    const docagricultor = db.ref("agricultor/" + id);
+    docagricultor.remove();
+    res.redirect("/agricultor");
 });
 
 module.exports = app;
