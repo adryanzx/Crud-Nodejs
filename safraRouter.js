924b836906895e9c73cd67bdfa1e08fc0d7b3abd
yan060604 <yan060604@gmail.com>
2024-06-21 17:44:49 -0300

    :sparkles: mostrando itens da tabela safra || :bug: não está mostrando o dado culturaNome e culturaTempo

--- safraRouter.js
+++ safraRouter.js
@@ -7,33 +7,68 @@ const app = express();
 const admin = require("./firebase");
 const db = admin.database();
 
+
+function criarTabela(dados) {
+    // let cultura = req.params.id;
+    let tabela = `<table class="table table-striped zebrado">
+                    <thead>
+                        <tr>
+                            <th>Data inicial: </th>
+                            <th>Data Final: </th>
+                            <th>Status:</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    for (let chave in dados) {  
+        tabela += `<tr>
+                        <td>${dados[chave].dataini}</td>
+                        <td>${dados[chave].datafim}</td>
+                        <td>${dados[chave].status}</td>
+                    </tr>`;
+        // for (let d in cultura) {   
+        //     tabela += `<tr>
+        //     <td colspan="3">${cultura[d].nome} - ${cultura[d].tempo}</td>
+        // </tr>`;         
+        // }
+    }
+    tabela += `</tbody >
+            </table > `;
+    return tabela;
+}
+
 app.get('/', (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) =>{
-        fs.readFile('src/rodape.html', (e, rodape) =>{
-            fs.readFile('src/safra/safra.html', (e, dados) =>{
-                res.writeHead(200, {"Content-Type": 'text/html'});
-                res.write(cabecalho+dados+rodape);
-                res.end();
+    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
+        fs.readFile('src/rodape.html', (e, rodape) => {
+            fs.readFile('src/safra/safra.html', (e, dados) => {
+                let tabela = "";
+                const docsafra = db.ref("safra");
+                docsafra.once("value", function (snapshot) {
+                    tabela = criarTabela(snapshot.val());
+                    dados = dados.toString().replace("{tabela}", tabela);
+                    res.writeHead(200, { "Content-Type": 'text/html' });
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-app.get('/novo', async (req,res) =>{
+app.get('/novo', async (req, res) => {
     const docsafra = db.ref("safra").push();
     const safra = {
         dataini: req.query.dataini,
         datafim: req.query.datafim,
-        status: "aberto",
-        tempo: ""
+        status: "aberto"
     };
     docsafra.set(safra);
-    res.json({id : docsafra.key });
+    res.json({ id: docsafra.key });
 });
 
-app.get('/cultura', async(req, res) => {
+app.get('/cultura', async (req, res) => {
     const doccultura = db.ref("cultura");
-    await doccultura.once("value", function(snapshot){
+    await doccultura.once("value", function (snapshot) {
         const cultura = snapshot.val();
         return res.json(cultura);
     })
@@ -41,8 +76,8 @@ app.get('/cultura', async(req, res) => {
 
 app.get("/cultura/:id", async (req, res) => {
     let cultura = req.params.id;
-    const doccultura = db.ref("cultura/"+cultura);
-    await doccultura.once("value", function(snapshot){
+    const doccultura = db.ref("cultura/" + cultura);
+    await doccultura.once("value", function (snapshot) {
         cultura = snapshot.val();
         res.json(cultura);
     });
@@ -53,13 +88,19 @@ app.get("/adicionarcultura/:safraId/:culturaId/:culturaNome/:culturaTempo", asyn
     let culturaId = req.params.culturaId;
     let culturaNome = req.params.culturaNome;
     let culturaTempo = req.params.culturaTempo;
-    const docsafra = db.ref("safra/"+safraId+"/cultura").push();
+    const docsafra = db.ref("safra/" + safraId + "/cultura").push();
     const cultura = {
         id: culturaId,
         nome: culturaNome,
-        cultura: culturaTempo
-        
+        tempo: culturaTempo
     }
+    if (dados[chave].cultura) {
+        Object.keys(dados[chave].cultura).forEach(culturaId => {
+            const cultura = dados[chave].cultura[culturaId];
+            tabela += `${cultura.nome}: ${cultura.tempo}<br>`;
+        });
+    }   
+    
     docsafra.set(cultura)
         .then(() => res.json({ sucesso: true }))
         .catch((error) => res.json({ sucesso: false }));
@@ -68,27 +109,27 @@ app.get("/adicionarcultura/:safraId/:culturaId/:culturaNome/:culturaTempo", asyn
 app.get('/excluircultura/:safraId/:culturaId', async (req, res) => {
     const safraId = req.params.safraId;
     const culturaId = req.params.culturaId;
-    const docsafra = db.ref('safra/'+safraId+'/cultura/'+culturaId);
+    const docsafra = db.ref('safra/' + safraId + '/cultura/' + culturaId);
     docsafra.remove()
-        .then( () => res.json({sucesso : true}) )
-        .catch( (error) => res.json({sucesso: false}) );
+        .then(() => res.json({ sucesso: true }))
+        .catch((error) => res.json({ sucesso: false }));
 });
 
 app.get('/fechar/:idsafra', async (req, res) => {
     const idsafra = req.params.idsafra;
-    const docsafra = db.ref('safra/'+idsafra);
+    const docsafra = db.ref('safra/' + idsafra);
     docsafra.once("value", (snapshot) => {
         const cultura = snapshot.val().cultura;
-        if (cultura){
+        if (cultura) {
             let total = 0;
             Object.keys(cultura).forEach(culturaId => {
                 const cultura = cultura[culturaId];
                 const tempo = parseFloat(cultura.tempo);
                 total += tempo;
             });
-            docsafra.update({status: "Fechado", tempo: total})
-                .then(() => res.json({sucesso: true}))
-                .catch((error) => res.json({sucesso: false}));
+            docsafra.update({ status: "Fechado", tempo: total })
+                .then(() => res.json({ sucesso: true }))
+                .catch((error) => res.json({ sucesso: false }));
         }
     });
 });
